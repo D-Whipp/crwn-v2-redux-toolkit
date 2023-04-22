0c91c0248e2a620b916af4dea351f61144ff27fd
David Whipple <dwhipp88@gmail.com>
2023-04-21 19:56:05 -0600

    setup stripe, wrote serverless payment intent function

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
 
 import App from './App';
-import { store, persistor } from './store/store';
+// import { store, persistor } from './store/store';
+import { store } from './store/store';
+import {stripePromise} from './utils/stripe/stripe.utils';
 
 import './index.scss';
 
@@ -18,7 +21,9 @@ render(
     <PersistGate loading={null} persistor={persistor}>
   */}
             <BrowserRouter>
-                <App />
+                <Elements stripe={stripePromise}>
+                    <App />
+                </Elements>
             </BrowserRouter>
             {/*
       </PersistGate>
