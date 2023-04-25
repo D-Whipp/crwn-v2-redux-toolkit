56bc298691219d3248184f3a319475a768d9612b
David Whipple <dwhipp88@gmail.com>
2023-04-24 22:21:07 -0600

    setup stripe, fixed bugs by naming env variables starting with REACT_APP_ and installed stripe dependency and finally running the app with netlify dev

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(
+    process.env.REACT_APP_STRIPE_SECRET_KEY
+);
 
 exports.handler = async (event) => {
     try {
@@ -19,7 +21,7 @@ exports.handler = async (event) => {
         console.log({ error });
 
         return {
-            status: 400,
+            statusCode: 400,
             body: JSON.stringify({ error }),
         };
     }
