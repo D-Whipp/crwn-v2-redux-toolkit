2281055f9fe5c1fe9424ef92feccaab3d5a120a5
David Whipple <dwhipp88@gmail.com>
2023-04-26 20:50:31 -0600

    updated favicon and navigation component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -12,39 +12,41 @@ import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import {
-  NavigationContainer,
-  NavLinks,
-  NavLink,
-  LogoContainer,
+    NavigationContainer,
+    NavLinks,
+    NavLink,
+    LogoContainer,
+    WebsiteName,
 } from './navigation.styles';
 
 const Navigation = () => {
-  const currentUser = useSelector(selectCurrentUser);
-  const isCartOpen = useSelector(selectIsCartOpen);
+    const currentUser = useSelector(selectCurrentUser);
+    const isCartOpen = useSelector(selectIsCartOpen);
 
-  return (
-    <Fragment>
-      <NavigationContainer>
-        <LogoContainer to='/'>
-          <CrwnLogo className='logo' />
-        </LogoContainer>
-        <NavLinks>
-          <NavLink to='/shop'>SHOP</NavLink>
+    return (
+        <Fragment>
+            <NavigationContainer>
+                <LogoContainer to="/">
+                    <CrwnLogo className="logo" />
+                    </LogoContainer>
+                    <WebsiteName>Empress Clothing</WebsiteName>
+                <NavLinks>
+                    <NavLink to="/shop">SHOP</NavLink>
 
-          {currentUser ? (
-            <NavLink as='span' onClick={signOutUser}>
-              SIGN OUT
-            </NavLink>
-          ) : (
-            <NavLink to='/auth'>SIGN IN</NavLink>
-          )}
-          <CartIcon />
-        </NavLinks>
-        {isCartOpen && <CartDropdown />}
-      </NavigationContainer>
-      <Outlet />
-    </Fragment>
-  );
+                    {currentUser ? (
+                        <NavLink as="span" onClick={signOutUser}>
+                            SIGN OUT
+                        </NavLink>
+                    ) : (
+                        <NavLink to="/auth">SIGN IN</NavLink>
+                    )}
+                    <CartIcon />
+                </NavLinks>
+                {isCartOpen && <CartDropdown />}
+            </NavigationContainer>
+            <Outlet />
+        </Fragment>
+    );
 };
 
 export default Navigation;
